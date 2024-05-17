15492ae904fdd0a8ec98337c3a456e53dca8cdef
wreulicke <wreulicke@gmail.com>
2024-05-17 22:02:53 +0900

    implement stacktrace

--- firews.go
+++ firews.go
@@ -1,34 +1,30 @@
 package fivews
 
 import (
-	"runtime/debug"
+	"runtime"
+	"strconv"
 	"strings"
 )
 
 type fivewsError struct {
 	message string
 	errs    []error
-	stack   *stack
-}
-
-// TODO stack traceの取り方見直す
-type stack struct {
-	stack []byte
+	stack   stack
 }
 
 func New(message string) error {
-	return &fivewsError{message: message, stack: &stack{stack: debug.Stack()}}
+	return &fivewsError{message: message, stack: caller()}
 }
 
 func Wrap(message string, cause error) error {
-	return &fivewsError{message: message, errs: []error{cause}, stack: &stack{stack: debug.Stack()}}
+	return &fivewsError{message: message, errs: []error{cause}, stack: caller()}
 }
 
 func Join(message string, cause1 error, cause2 error, causes ...error) error {
 	errs := make([]error, 0, 2+len(causes))
 	errs = append(errs, cause1, cause2)
 	errs = append(errs, causes...)
-	return &fivewsError{message: message, errs: errs, stack: &stack{stack: debug.Stack()}}
+	return &fivewsError{message: message, errs: errs, stack: caller()}
 }
 
 func LastMessage(err error) (bool, string) {
@@ -55,7 +51,7 @@ func (e *fivewsError) Error() string {
 		}
 	}
 	b.WriteRune('\n')
-	b.Write(e.stack.stack)
+	b.WriteString(e.stack.String())
 	return b.String()
 }
 
@@ -76,4 +72,60 @@ func (e *fivewsError) String() string {
 
 func (e *fivewsError) Unwrap() []error {
 	return e.errs
+}
+
+type stack []uintptr
+
+func caller() stack {
+	var cs [32]uintptr
+	n := runtime.Callers(3, cs[:])
+	return stack(cs[0:n])
+}
+
+type frame struct {
+	pc uintptr
+}
+
+func (s stack) Frames() []frame {
+	frames := make([]frame, len(s))
+	for i, pc := range s {
+		frames[i] = frame{pc}
+	}
+	return frames
+}
+
+func (s stack) String() string {
+	var b strings.Builder
+	for _, frame := range s.Frames() {
+		b.WriteString(frame.String())
+		b.WriteRune('\n')
+	}
+	return b.String()
+}
+
+func (f frame) Function() string {
+	fn := runtime.FuncForPC(f.pc)
+	if fn == nil {
+		return "unknown"
+	}
+	return fn.Name()
+}
+
+func (f frame) FileLine() (string, int) {
+	fn := runtime.FuncForPC(f.pc)
+	if fn == nil {
+		return "unknown", 0
+	}
+	file, line := fn.FileLine(f.pc)
+	return file, line
+}
+
+func (f frame) String() string {
+	fn := runtime.FuncForPC(f.pc)
+	if fn == nil {
+		return "unknown"
+	}
+
+	file, line := fn.FileLine(f.pc)
+	return fn.Name() + " " + file + ":" + strconv.Itoa(line)
 }
