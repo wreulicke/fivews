628b17edaad1d1958422ef20bf254405f9b8729b
wreulicke <wreulicke@gmail.com>
2024-05-17 22:07:47 +0900

    add test

--- fivews_test.go
+++ fivews_test.go
@@ -1,17 +1,35 @@
 package fivews
 
 import (
+	"strings"
 	"testing"
 )
 
+func firstLine(err error) string {
+	m := err.Error()
+	return m[:strings.Index(m, "\n")]
+}
+
 func TestNew(t *testing.T) {
-	t.Error(New("error"))
+	if firstLine(New("error")) != "error" {
+		t.Error("New error message is not correct")
+	}
 }
 
 func TestWrap(t *testing.T) {
-	t.Error(Wrap("wrap", New("cause")))
+	if firstLine(Wrap("wrap", New("cause"))) != "wrap: cause" {
+		t.Error("Wrap error message is not correct")
+	}
 }
 
 func TestJoin(t *testing.T) {
-	t.Error(Join("wrap", New("cause1"), Wrap("test", New("cause2"))))
+	if firstLine(Join("wrap", New("cause1"), Wrap("test", New("cause2")))) != "wrap: cause1: test: cause2" {
+		t.Error("Join error message is not correct")
+	}
+}
+
+func TestLastMessage(t *testing.T) {
+	if _, m := LastMessage(Wrap("wrap", New("error"))); m != "wrap" {
+		t.Error("LastMessage error message is not correct")
+	}
 }
